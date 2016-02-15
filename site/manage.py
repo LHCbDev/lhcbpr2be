25b517d72a94555fdd2840b4dad6df477c894193
Alexander Mazurov <alexander.mazurov@gmail.com>
2016-02-15 11:00:03 +0100

    Put reading environment to manage.py

--- site/manage.py
+++ site/manage.py
@@ -2,10 +2,17 @@
 import os
 import sys
 import re
+import dotenv
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    os.environ.setdefault("ENV", "default")
+
+    env_file = os.path.abspath(os.path.join('envs',"%s.env" % os.getenv('ENV')))
+    print('*' * 80)
+    print("Read environment from '{}'".format(env_file))
+    print('*' * 80)
+    dotenv.read_dotenv(env_file)
 
     from django.core.management import execute_from_command_line
-    # read_env()
     execute_from_command_line(sys.argv)
