7d15cb072f79a1cadb15612d74afe70dacbfa8e0
Alexander Mazurov <alexander.mazurov@gmail.com>
2016-04-06 15:35:13 +0200

    lhcbpr2be: change url for API documentation to /api/docs

--- site/urls.py
+++ site/urls.py
@@ -95,7 +95,7 @@ urlpatterns = [
 
     url(r'^api-auth/',
         include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^docs/', include('rest_framework_swagger.urls')),
+    url(r'^api/docs/', include('rest_framework_swagger.urls')),
     url(r'^$', RedirectView.as_view(url='/api', permanent=True)),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
